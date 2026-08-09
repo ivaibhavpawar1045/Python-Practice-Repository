c0bafde3ee081f040d84b7e80fbdcfae33e076b3
Vaibhav Pawar <vaibhavpawar1045@gmail.com>
2026-08-09 22:42:15 +0530

    Solved Selfstudy Python : Use of while loop

---
+++ while_loop.py
@@ -0,0 +1,20 @@
+
+
+"""
+Problem:  Use of while loop
+
+Sample Input : User given
+
+Platform: Self study
+Difficulty: Easy 
+
+"""
+
+
+while True:
+     num = int(input("enter the number :  "))
+     print("your entered number" ,num)
+
+     choice= input("enter your choice(y/n)")
+     choice.lower() != 'y'
+     break
