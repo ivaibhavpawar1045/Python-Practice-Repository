759d1c413f49d89ba8dc0b71ac26059be65ed479
Vaibhav Pawar <vaibhavpawar1045@gmail.com>
2026-08-23 19:42:23 +0530

    Solved SelfStudy Python : Find the number is Even or Odd

---
+++ even_odd.py
@@ -0,0 +1,17 @@
+"""
+Problem:  Write program to idetify whether a number is even or odd
+
+Sample Input : null
+
+Platform: SelfStudy
+
+Difficulty: easy
+
+"""
+
+number = int(input("Enter your number : " ))
+
+if (number % 2 == 0):
+     print("Number is Even")
+else:
+     print("Number is Odd")
