fc66a633065f91a058eb28523e596fb8c16adbb0
Vaibhav Pawar <vaibhavpawar1045@gmail.com>
2026-08-25 22:36:56 +0530

    Solved SelfStudy Python : Simple Calculations

---
+++ simple_clalculation.py
@@ -0,0 +1,19 @@
+"""
+Problem:  Write simple calculation program
+
+Sample Input : User define
+
+Platform: SelfStudy
+
+Difficulty: easy
+
+"""
+
+
+i = int(input("Enter 1st number : "))
+j = int(input("Enter 2nd number : "))
+
+print("Addition is :" , i+j)
+print("Substraction is :" , i-j)
+print("Multiplication is :" , i*j)
+print("Division is :" , i/j)
